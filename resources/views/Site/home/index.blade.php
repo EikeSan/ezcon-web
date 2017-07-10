23fbca538bb6e3987b64815b4f2d875d9fb6f811
EikeSan <eikesantz@hotmail.com>
2017-07-09 22:30:57 -0300

    Ajustes no background

--- resources/views/Site/home/index.blade.php
+++ resources/views/Site/home/index.blade.php
@@ -1,33 +1,30 @@
 @extends('Site.templates.template')
 @section('content')
   @include('Site.includes.navbar')
-  <div class="cover">
-    <div class="navbar">
+  <div class="container">
+    <div class="cover">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-11 text-center">
-          <img width="250px" height="250px" src="{{url('assets/Site/img/ezconLogo.png')}}" align="left-botton">
-          <p class="text-inverse">
-            <br>
-            <br>
-            <br>
-            <b style="color: black">Seu condomínio a um clique de você.</b>
-          </p>
-          <a href="{{route('login')}}" class="btn btn-lg btn-primary">Login</a>
+        <div class="row">
+          <div class="col-md-11 text-center">
+            <img width="30%" height="30%" src="{{url('assets/Site/img/ezconLogo.png')}}">
+            <p class="text-inverse">
+              <br>
+              <br>
+              <b style="color: black">Seu condomínio a um clique de você.</b>
+            </p>
+            <a href="{{route('login')}}" class="btn btn-lg btn-primary">Login</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
+  <style type="text/css">
+    body{
+      background: url(https://blog.nextin.com.br/wp-content/uploads/2015/04/IPTU-unidades-e-condom%C3%ADnio.png) no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+  </style>
 @endsection
