df80a6244188fb6c401f8d358743c80fcab97493
KarllaSouza <karlladanielly94@hotmail.com>
2017-07-19 20:44:01 -0300

    remoção atributo unique de name em validação de formulário
    Signed-off-by: KarllaSouza <karlladanielly94@hotmail.com>

--- app/Http/Requests/UserFormRequest.php
+++ app/Http/Requests/UserFormRequest.php
@@ -24,7 +24,7 @@ class UserFormRequest extends FormRequest
     public function rules()
     {
         return [
-          'name' => 'required|unique|string|max:255',
+          'name' => 'required|string|max:255',
           'email' => 'required|string|email|max:255|unique:users',
           'password' => 'required|string|min:6|confirmed',
           'type' => 'required',
